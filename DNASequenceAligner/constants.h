da2dc640be6efb082d58b379188638562953eb14
alex <alex@TUF>
2015-08-01 23:11:46 -0600

    Se agregan las funciones de manejo de archivos y de manejo de secuencias de ADN

--- DNASequenceAligner/constants.h
+++ DNASequenceAligner/constants.h
@@ -8,5 +8,11 @@
 #define MATCH 1
 #define MISMATCH -1
 #define GAP -2
+#define D -5
+#define INPUT_FIRST_FILENAME "Ingrese el nombre del primer archivo (incluendo su extencion)"
+#define INPUT_SECOND_FILENAME "Ingrese el nombre del Segundo archivo (incluendo su extencion)"
+#define COMMON_EXTENSION ".dna"
+#define EXTENTION_ERROR "Wrong file or file type"
+
 
 #endif //DNASEQUENCEGENERATOR_CONSTANTS_H
